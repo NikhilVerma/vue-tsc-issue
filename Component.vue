f85ea44f6e98695b8d8f03472f412bd8c6abc115
Nikhil Verma <nikhilgotmail@gmail.com>
2022-03-02 22:43:21 +0530

    Example of volar/issues/1001

---
+++ Component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div @click="selectTab">hello</div>
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+
+@Component
+export default class Comp extends Vue {
+  selectTab() {
+    this.$emit("select", 1);
+  }
+}
+</script>
---
+++ Consumer.vue
@@ -0,0 +1,18 @@
+<template>
+  <Component @select="selectTab" />
+</template>
+
+<script lang="ts">
+import Component from "vue-class-component";
+
+@Component({
+  components: {
+    Component,
+  },
+})
+export default class Consumer extends Vue {
+  selectTab(value: number) {
+    console.log(value);
+  }
+}
+</script>
